464ca03b0dd761e9ac92a353659991944f6d0578
LeonardoMachiavelli <leonardo_jibran@zensa.com.br>
2025-06-03 17:38:02 -0300

    test

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ export default function HomePage() {
 		<div>
 			<h2 className="text-white text-2xl font-semibold">Welcome to the Home Page</h2>
 			<p className="mt-2 text-gray-600">This page uses the AppLayout with navbar.</p>
+			<p className="mt-2 text-gray-600">Test deploy CI/CD</p>
 			<button className="hover:bg-blue-600 text-white rounded p-2 bg-blue-500">Test</button>
 		</div>
 	);
